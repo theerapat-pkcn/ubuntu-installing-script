7c913d7ccf6c089f410e372290e3e4b8c7b5da5b
theerapat-pkcn <theerapat.pkcn@gmail.com>
2022-07-02 03:05:39 +0700

    main

--- ubuntu_server_init_install/install.sh
+++ ubuntu_server_init_install/install.sh
@@ -8,13 +8,14 @@ apt-get update && apt-get upgrade -y
 # Set timezone to Asia/Bangkok
 timedatectl set-timezone Asia/Bangkok
 
-# Create new user
-adduser bai
+echo "Create new user"
+#adduser bai
+adduser --disabled-password --gecos "" bai
 
-# Granting Administrative Privileges
+echo "Granting Administrative Privileges"
 usermod -aG sudo bai
 
-# Replicate ssh from root
+echo "Replicate ssh from root"
 rsync --archive --chown=bai:bai ~/.ssh /home/bai
 
 su bai
@@ -23,6 +24,8 @@ sleep 5
 sudo -n true
 test $? -eq 0 || exit 1 "you should have sudo privilege to run this script"
 
+
+echo "Installing docker"
 sudo apt update
 sudo apt install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
@@ -32,17 +35,20 @@ sudo apt install -y docker-ce docker-ce-cli containerd.io
 sudo usermod -aG docker $USER
 sudo apt install -y docker-compose
 
+echo "Installing git"
 sudo apt-get install git -y
 
+echo "Installing ufw"
 sudo apt install ufw -y
 sudo ufw default deny incoming
 sudo ufw default allow outgoing
 sudo ufw allow ssh
 sudo ufw allow 22
 sudo ufw allow 2222
-sudo ufw enable
+sudo ufw enable -y
 
 #---- powerline font installation
+echo "Installing Powerline-fonts"
 mkdir -p $HOME/downloads
 cd $HOME/downloads
 git clone https://github.com/powerline/fonts.git --depth=1
@@ -57,11 +63,12 @@ cd
 
 
 #---- nvim installation
+echo "Installing nvim"
 sudo apt-get install neovim -y
 
 # Create folder in not exist
 mkdir -p $HOME/.config
 mkdir -p $HOME/.config/nvim
 
-cp ./nvim-init.conf $HOME/.config/nvim/init.vim
+cp nvim-init.conf $HOME/.config/nvim/init.vim
 #----
